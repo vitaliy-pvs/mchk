3a983e795166a95f452c010a2fd7b55b11ce7915
pvs <vitaliypyhodko@gmail.com>
2021-09-23 08:30:11 +0300

    added imgindent page

--- sizes/urls.py
+++ sizes/urls.py
@@ -3,11 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.mchk_01, name='mchk_01'),
-    # path('mchk_01_OS', views.mchk_01_OS, name='mchk_01_OS'),
-    # path('mchk_04', views.mchk_04, name='mchk_04'),
-    # path('mchk_04_OS', views.mchk_04_OS, name='mchk_04_OS'),
-
-    # path('pagelist/<int:pk>/', views.page_list, name='page_list'),
-    # path('page/<int:pk>/', views.page, name='page'),
-    # path('menu/<int:pk>/', views.menu_page, name='menu_page'),
+    path('imgident/', views.imgident, name='imgident'),
+    path('imgident/translate/', views.translate, name='translate'),
 ]
--- sizes/views.py
+++ sizes/views.py
@@ -1,8 +1,8 @@
 import os
-
 from django.shortcuts import render
-
 from mchk.settings import STATIC_ROOT
+from deep_translator import GoogleTranslator
+from django.http import JsonResponse
 
 STATIC_ROOT = STATIC_ROOT.replace("\\", "/")
 
@@ -20,7 +20,7 @@ if os.path.isfile(os.path.join(STATIC_ROOT, 'header.jpg')):
 
 page_title_list = [
     ["МШК-01", "mchk_01"],
-    # ["МШК-01.ОС", "mchk_01_OS"],
+    ["imgident", "imgident"],
     # ["МШК-04", "mchk_04"],
     # ["МШК-04.ОС", "mchk_04_OS"],
 ]
@@ -34,14 +34,19 @@ def mchk_01(request):
     })
 
 
-# def mchk_01_OS(request):
-#     current_page_title = "МШК-01.ОС"
-#     return render(request, 'mchk_01_OS.html', {
-#         'current_page_title': current_page_title,
-#         'page_title_list': page_title_list,
-#     })
-#
-#
+def imgident(request):
+    current_page_title = "imgident"
+    return render(request, 'imgident.html', {
+        'current_page_title': current_page_title,
+        'page_title_list': page_title_list,
+    })
+
+def translate(request):
+    response = {
+        'translate_str': GoogleTranslator(source='en', target='ru').translate(request.GET.get('text', None))
+    }
+    return JsonResponse(response)
+
 # def mchk_04(request):
 #     current_page_title = "МШК-04"
 #     return render(request, 'mchk_04.html', {
